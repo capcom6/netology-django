69987a14cbfafa3823c2f4072ca5450602b031f2
Aleksandr Soloshenko <capcom2me@gmail.com>
2023-04-01 16:11:41 +0700

    Added solution for m2m-relations

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,8 +1,34 @@
 from django.contrib import admin
+from django import forms
 
-from .models import Article
+from .models import Article, Tag, Scope
+
+
+class ScopeInlineFormset(forms.BaseInlineFormSet):
+    def clean(self) -> None:
+        has_main_scope = False
+        for form in self.forms:
+            if has_main_scope and form.cleaned_data.get("is_main", False):
+                raise forms.ValidationError(
+                    "Статья может иметь только один основной раздел"
+                )
+
+            has_main_scope = has_main_scope or form.cleaned_data.get("is_main", False)
+        if not has_main_scope:
+            raise forms.ValidationError("Не указан основной раздел")
+        return super().clean()  # вызываем базовый код переопределяемого метода
+
+
+class ScopeInline(admin.TabularInline):
+    model = Scope
+    formset = ScopeInlineFormset
 
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
+    inlines = (ScopeInline,)
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
     pass
